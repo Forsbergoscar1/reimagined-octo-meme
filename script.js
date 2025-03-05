edf9ade615c6e85b2b1258abfdfe606442e2684d
Forsbergoscar1 <forsbergoscar1@gmail.com>
2025-03-05 21:30:54 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ const database = {
     "21 20 6 29 18 20": "Utfört",
     "42941115191912518": "Döda Kessler",
     "4 29 4 1 11 5 19 19 12 5 18": "Döda Kessler",
-    "XN-27 T-P F3 A No XCH": "Särskild sändebud ankommer Hamburg–Paris natten 13-14 oktober. Säkerställ mottagande och vidare bekräftelse. Ingen extern kommunikation."
+    "XN-27 T-P F3 A No XCH": "Särskild sändebud ankommer Hamburg–Paris tidig morgon 14 oktober. Säkerställ mottagande och vidare bekräftelse. Ingen extern kommunikation.",
+    "X 14 K9T7 2ZP0": "X-14: Säkerhetsklassad enhet aktiv. Specialuppdrag bekräftat.",
+
+
 };
 
 const searchBox = document.getElementById("searchBox");
